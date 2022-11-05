593c1358dd99c5ce1c5adac1fda35dd3c95d02b3
Vitaly Perov <vitperov@gmail.com>
2022-11-05 20:30:27 +0200

    Save model to json

--- modules/Model.py
+++ modules/Model.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from PyQt5.QtCore import *
 
+import json
+
 class HouseSquareType(Enum):
     Empty   = 0
     Corner  = 1
@@ -48,12 +50,30 @@ class HouseMapSquareModel(QObject):
     def getProperty(self, name):
         return self.properties[name]
 
+    def toJson(self):
+        # properties are enums, they can't be directly converted to int
+        props = dict()
+        for name, prop in self.properties.items():
+            props[name] = prop.value
+
+        props['id'] =  self.id
+        return json.dumps(props)
+        
+    def toSerializableObj(self):
+        # properties are enums, they can't be directly converted to int
+        obj = dict()
+        for name, prop in self.properties.items():
+            obj[name] = prop.value
+
+        obj['id'] =  self.id
+        return obj
+
 
 class HouseMapModel(QObject):
     updatedEntireMap = pyqtSignal()
     def __init__(self):
         QObject.__init__(self)
-        
+
         self.width = 0
         self.height = 0
         self._squares = dict()
@@ -66,7 +86,7 @@ class HouseMapModel(QObject):
             for column in range(w):
                 id = row*1000 + column
                 self._squares[id] = HouseMapSquareModel(id)
-                
+
         self.updatedEntireMap.emit()
 
     def getSquare(self, id):
@@ -78,9 +98,27 @@ class HouseMapModel(QObject):
     def size(self):
         return [self.height, self.width]
 
+    def toSerializableObj(self):
+        squares = dict()
+        for id, square in self._squares.items():
+            squares[id] = square.toSerializableObj()
+
+        obj = dict()
+        obj['version'] = 1
+        obj['squares'] = squares
+
+        return obj
+
     def saveMap(self, filename):
+        extension = '.house'
+        if not filename.endswith(extension):
+            filename = filename + extension
+
         print("Saving map to " + filename)
-        
+        with open(filename, "w") as writeFile:
+            json.dump(self.toSerializableObj(), writeFile, indent=4)
+
+
     def loadMap(self, filename):
         print("Loading map to " + filename)
         self.updatedEntireMap.emit()
