f4fc7c66caf4a3a96531256a8cb4ebab8b8209de
Vitaly Perov <vitperov@gmail.com>
2023-10-05 19:49:44 +0300

    Move part of GeneratorSettingsDlg code to the new class ClassVariablesGuiEditor

---
+++ modules/ClassVariablesGuiEditor.py
@@ -0,0 +1,70 @@
+from PyQt5 import QtWidgets
+from pyqtgraph.Qt import QtCore, QtGui
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+
+def is_class(o):
+    return hasattr(o, '__dict__')
+
+class ClassVariablesGuiEditor():
+    class DynamicItem():
+        def __init__(self, name, label, convertFunc, varStoragePtr):
+            self._name = name
+            self._label = label
+            self._layout = None
+            self._convertFunc = convertFunc
+
+            self._editWg = None
+
+            self._varStoragePtr = varStoragePtr
+
+            self._createLayout()
+
+        def _createLayout(self):
+            self._layout = QHBoxLayout()
+            label = QLabel(self._label)
+            self._editWg = QLineEdit()
+            self._layout.addWidget(label)
+            self._layout.addWidget(self._editWg)
+            val = getattr(self._varStoragePtr, self._name)
+            self._editWg.setText(str(val))
+
+            def onTextChanged(txt):
+                try:
+                    value = self._convertFunc(txt)
+                    setattr(self._varStoragePtr, self._name, value)
+                except:
+                    pass
+
+            self._editWg.textChanged.connect(onTextChanged)
+
+        def getLayout(self):
+            return self._layout
+ 
+    # NOTE: we do not store anything iside the class, but let it be
+    # the class method insted of just functin (perhaps we will need it in the future)
+    
+    # FIXME: prefix parameter is needed only for the debug purposes. It can be deleted
+    def createControls(self, cls, prefix, layout):
+        for attr in dir(cls):
+            if attr.startswith("__"):
+                continue
+            val = getattr(cls, attr)
+            if callable(val):
+                continue
+
+            if is_class(val):
+                print(prefix + attr + "->");
+                nestedLayout = QVBoxLayout()
+                box = QGroupBox(attr)
+                box.setLayout(nestedLayout)
+                #layout.addLayout(nestedLayout)
+                layout.addWidget(box)
+                self.createControls(val, prefix + "    ", nestedLayout)
+                continue
+
+            print(prefix + attr + "->" + str(val))
+            item = self.DynamicItem(attr, attr, type(val), cls)
+            layout.addLayout(item.getLayout())
--- modules/GeneratorSettingsDlg.py
+++ modules/GeneratorSettingsDlg.py
@@ -4,49 +4,16 @@ from pyqtgraph.Qt import QtCore, QtGui
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
-#import inspect
-#from collections import namedtuple
 import json
 from json import JSONEncoder
 
-from modules.GeneratorSettings import *
+from modules.GeneratorSettings       import *
+from modules.ClassVariablesGuiEditor import *
 
 class GeneratorSettingsEncoder(JSONEncoder):
         def default(self, o):
             return o.__dict__
 
-
-def is_class(o):
-    return hasattr(o, '__dict__')
-    
-class DynamicItem():
-    def __init__(self, name, label, convertFunc, varStoragePtr):
-        self._name = name
-        self._label = label
-        self._layout = None
-        self._convertFunc = convertFunc
-
-        self._editWg = None
-        
-        self._varStoragePtr = varStoragePtr
-
-        self._createLayout()
-
-    def _createLayout(self):
-        self._layout = QHBoxLayout()
-        label = QLabel(self._label)
-        self._editWg = QLineEdit()
-        self._layout.addWidget(label)
-        self._layout.addWidget(self._editWg)
-        val = getattr(self._varStoragePtr, self._name)
-        self._editWg.setText(str(val))
-
-    def getLayout(self):
-        return self._layout
-
-
-#def GeneratorSettingsDecoder(inputDict):
-#    return namedtuple('X', inputDict.keys())(*inputDict.values())
 
 class GeneratorSettingsDlg(QtGui.QDialog):
     def __init__(self):
@@ -73,18 +40,9 @@ class GeneratorSettingsDlg(QtGui.QDialog):
         loadSaveLayout.addWidget(saveButton)
 
         layout.addLayout(loadSaveLayout)
-        
+
         self.dynamicLayout = QVBoxLayout()
         layout.addLayout(self.dynamicLayout)
-
-        #label = QLabel('Do not forget to push "Save" on the main window')
-        #layout.addWidget(label)
-        #label.setStyleSheet("QLabel { color : red; qproperty-alignment: AlignCenter;}");
-
-        #varNames = ['calibration', 'speed20ma', 'amplitude20ma']
-        #panel = VariablesPanelWidget()
-        #panel.populateModelVariables(model, varNames)
-        #layout.addWidget(panel)
 
         self.setLayout(layout)
 
@@ -95,7 +53,7 @@ class GeneratorSettingsDlg(QtGui.QDialog):
             filename = filename + extension
 
         print("Saving settings to " + filename)
-        with open(filename, "w") as writeFile:
+        with open(filename, "w+") as writeFile:
             jsonObj = json.dumps(self.settings, indent=4, cls=GeneratorSettingsEncoder)
             writeFile.write(jsonObj)
 
@@ -111,39 +69,17 @@ class GeneratorSettingsDlg(QtGui.QDialog):
             settingsDict = json.loads(jsStr)
             self.settings.loadFromDict(settingsDict)
 
-    def createControlsByClassVariables(self, var, prefix, layout):
-        for attr in dir(var):
-            if attr.startswith("__"):
-                continue
-            val = getattr(var, attr)
-            if callable(val):
-                continue
-
-            if is_class(val):
-                print(prefix + attr + "->");
-                nestedLayout = QVBoxLayout()
-                box = QGroupBox(attr)
-                box.setLayout(nestedLayout)
-                #layout.addLayout(nestedLayout)
-                layout.addWidget(box)
-                self.createControlsByClassVariables(val, prefix + "    ", nestedLayout)
-                continue
-
-            print(prefix + attr + "->" + str(val))
-            item = DynamicItem(attr, attr, type(val), var)
-            layout.addLayout(item.getLayout())
-
-
     def onSavePressed(self):
         self.saveToFile('testSettings.json')
 
 
     def onLoadPressed(self):
         self.loadFromFile('testSettings.json')
-        print(self.settings.__dict__)
-        print(self.settings.zoneSettings.__dict__)
-        print("----------")
-        self.createControlsByClassVariables(self.settings, "", self.dynamicLayout)
+        #print(self.settings.__dict__)
+        #print(self.settings.zoneSettings.__dict__)
+        #print("----------")
+        edt = ClassVariablesGuiEditor();
+        edt.createControls(self.settings, "", self.dynamicLayout)
 
 
     @staticmethod
