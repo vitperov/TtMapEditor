022c35c042ca8197918c7a506d098a115bf560a0
Vitaly Perov <vitperov@gmail.com>
2024-07-14 10:34:26 +0300

    Support generators properties

--- modules/GeneratorPluginBase.py
+++ modules/GeneratorPluginBase.py
@@ -1,8 +1,30 @@
+import os
+import inspect
+
 from modules.MapModelGeneral import *
 
 class GeneratorPluginBase:
     def __init__(self, mapModel):
         self.mapModel = mapModel
+        self.pluginSettings = {}
+        self.schema = {}
+        #childPluginClassName = self.__class__.__name__
+        childPluginLocation = inspect.getfile(self.__class__);
+        self.settingsFileName = os.path.splitext(childPluginLocation)[0] + '.json'
+        self.loadPluginSettings2()
+
+    def loadPluginSettings2(self):
+        with open(self.settingsFileName, 'r') as f:
+            #print("FIle content:" + f.read())
+            self.pluginSettings = json.load(f)
+            print(">SETTINGS: " + str(self.pluginSettings))
+            self.schema = self.pluginSettings['schema']
+            
+            print(">SCHEMA: " + str(self.schema))
+
+    #def loadPluginSettings(self):
+    #    with open(self.settingsFileName, 'w') as f:
+    #        json.dump(self.pluginSettings, f, indent=4)
 
     def generate(self, settings):
         raise NotImplementedError("Each plugin must implement the run method.")
--- modules/GeneratorsPanel.py
+++ modules/GeneratorsPanel.py
@@ -7,6 +7,64 @@ from PyQt5.QtCore import pyqtSignal, pyqtSlot
 
 from functools import partial
 
+class GeneratorSettings(QWidget):
+    def __init__(self, model):
+        QWidget.__init__(self)
+        self.model = model
+        self.settings = {}
+        self.controls = {}
+        self.initGui()
+
+    def initGui(self):
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        print("SCHEMA: " + str(self.model.schema))
+
+        for key, info in self.model.schema.items():
+            controlLayout = QHBoxLayout()
+            label = QLabel(key)
+            controlLayout.addWidget(label)
+
+            settingType = info['type']
+            settingValue = info['value']
+            
+            if settingType == 'str':
+                control = QLineEdit()
+                control.setText(settingValue)
+            elif settingType == 'int':
+                control = QSpinBox()
+                control.setValue(int(settingValue))
+            elif settingType == 'float':
+                control = QDoubleSpinBox()
+                control.setDecimals(2)
+                control.setRange(-1e6, 1e6)  # Set an appropriate range for your needs
+                control.setValue(float(settingValue))
+            elif settingType == 'bool':
+                control = QCheckBox()
+                control.setChecked(int(settingValue))
+            else:
+                raise Exception("Unsupported schema type: " + settingType)
+            
+            controlLayout.addWidget(control)
+            layout.addLayout(controlLayout)
+            self.controls[key] = control
+
+        appyButton = QPushButton("Appy")
+        appyButton.clicked.connect(self.applySettings)
+        layout.addWidget(appyButton)
+
+    def applySettings(self):
+        for key, control in self.controls.items():
+            if isinstance(control, QLineEdit):
+                self.settings[key]['value'] = control.text()
+            elif isinstance(control, QSpinBox):
+                self.settings[key]['value'] = control.value()
+            elif isinstance(control, QDoubleSpinBox):
+                self.settings[key]['value'] = control.value()
+            elif isinstance(control, QCheckBox):
+                self.settings[key]['value'] = control.isChecked()
+
 class GeneratorItem(QWidget):
     def __init__(self, name, model):
         QWidget.__init__(self)
@@ -14,9 +72,13 @@ class GeneratorItem(QWidget):
         self.model = model;
         self.settings = {}
         
-        layout = QHBoxLayout()
+        layout = QVBoxLayout()
         self.setLayout(layout)
-        
+        firstRowLayout = QHBoxLayout()
+        settingsWg = GeneratorSettings(model)
+        layout.addLayout(firstRowLayout)
+        layout.addWidget(settingsWg)
+
         nameLabel = QLabel(name)
         layout.addWidget(nameLabel)
         
