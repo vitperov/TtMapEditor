3417b251d547d7df62b05eb7ae3750e9e650d9ba
Vitaly Perov <vitperov@gmail.com>
2024-07-24 22:06:07 +0300

    Add open and save icons

--- modules/Terrain/TerrainToolbar.py
+++ modules/Terrain/TerrainToolbar.py
@@ -1,10 +1,6 @@
-from PyQt5 import QtWidgets
-from pyqtgraph.Qt import QtCore, QtGui
-
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-
-from functools import partial
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton, QFileDialog
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtGui import QIcon
 
 class TerrainToolbar(QWidget):
     mapSettings  = pyqtSignal()
@@ -13,17 +9,19 @@ class TerrainToolbar(QWidget):
     refreshMap   = pyqtSignal()
 
     def __init__(self):
-        QWidget.__init__(self)
+        super().__init__()
         self._layout = QHBoxLayout()
-
         self.setLayout(self._layout)
 
-        self._settingsBtn   = QPushButton("Generator settings");
+        self._settingsBtn   = QPushButton("Generator settings")
         self._openBtn       = QPushButton("Open")
         self._saveBtn       = QPushButton("Save")
         self._refreshButton = QPushButton("Refresh", self)
-        self._refreshButton.setIcon(QtGui.QIcon.fromTheme("view-refresh"))
         
+        self._openBtn.setIcon(QIcon.fromTheme("document-open"))
+        self._saveBtn.setIcon(QIcon.fromTheme("document-save"))
+        self._refreshButton.setIcon(QIcon.fromTheme("view-refresh"))
+
         self._layout.addWidget(self._settingsBtn)
         self._layout.addWidget(self._openBtn)
         self._layout.addWidget(self._saveBtn)
@@ -40,15 +38,14 @@ class TerrainToolbar(QWidget):
         self.mapSettings.emit()
 
     def _saveFile(self):
-        name = QtGui.QFileDialog.getSaveFileName(self, 'Save File', filter='*.json')
-        name = name[0]
-        print(name)
-        self.loadMap.emit(name)
+        name, _ = QFileDialog.getSaveFileName(self, 'Save File', filter='*.json')
+        if name:
+            self.saveMap.emit(name)
 
     def _openFile(self):
-        name = QtGui.QFileDialog.getOpenFileName(self, 'Open File', filter='*.json')
-        name = name[0]
-        self.openMap.emit(name)
+        name, _ = QFileDialog.getOpenFileName(self, 'Open File', filter='*.json')
+        if name:
+            self.openMap.emit(name)
         
     def _refreshAction(self):
         self.refreshMap.emit()
