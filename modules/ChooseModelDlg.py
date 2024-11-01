9bf4c9f4f32d80a60bcb0e912274bac46edd878d
Vitaly Perov <vitperov@gmail.com>
2024-11-01 21:34:56 +0200

    ChooseModelDlg: support different categories

--- modules/ChooseModelDlg.py
+++ modules/ChooseModelDlg.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QPushButton, QLabel, QGridLayout
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QPushButton, QLabel, QGridLayout, QTabWidget, QWidget, QSpacerItem, QSizePolicy
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import QSize
 from functools import partial
@@ -10,8 +10,26 @@ class ChooseModelDlg(QDialog):
         self.selectedModel = None
         self._tilesize = tilesize
         
-        layout = QGridLayout()
+        self.objCollection = objCollection
+
+        layout = QVBoxLayout()
         self.setLayout(layout)
+
+        self.tabWidget = QTabWidget()
+        layout.addWidget(self.tabWidget)
+        
+        categories = [cat for cat in objCollection.allObjectCategories() if cat != "hidden"]
+        
+        for cat in categories:
+            tab = QWidget()
+            self.tabWidget.addTab(tab, cat)
+            self.populateTab(tab, cat, objCollection)
+
+        self.tabWidget.setCurrentIndex(categories.index(category))
+
+    def populateTab(self, tab, category, objCollection):
+        layout = QGridLayout()
+        tab.setLayout(layout)
         
         availableObjects = objCollection.getTypesInCategory(category)
         
@@ -31,7 +49,12 @@ class ChooseModelDlg(QDialog):
             itemLayout.addWidget(btn)
             
             layout.addLayout(itemLayout, row, col)
-    
+        
+        last_row = (len(availableObjects) // col_count) + 1
+        verticalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        layout.addItem(verticalSpacer, last_row, 0, 1, col_count)
+
+
     def updatePixmap(self, button, objCollection, model):
         imgFile = objCollection.getIcon(model)
         try:
