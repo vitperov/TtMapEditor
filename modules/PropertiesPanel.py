ee1e15b1536e3443664e5b6200c20c7710acfc7f
Vitaly Perov <vitperov@gmail.com>
2024-11-01 11:55:55 +0200

    Show intem picture on the PropertiesPanel

--- modules/PropertiesPanel.py
+++ modules/PropertiesPanel.py
@@ -6,15 +6,55 @@ from PyQt5.QtWidgets import *
 
 from functools import partial
 
+class SimpleSquareItem(QWidget):
+    def __init__(self, model, objCollection, tilesize):
+        super(SimpleSquareItem, self).__init__()
+        self._model = model
+        self._objCollection = objCollection
+        self._tilesize = tilesize
+        
+        self.initUI()
+
+    def initUI(self):
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        
+        label = QLabel()
+        self.updatePixmap(label)
+        
+        layout.addWidget(label)
+    
+    def updatePixmap(self, label):
+        sqType = self._model.getProperty('model')
+        rotation = self._model.getProperty('rotation')
+
+        imgFile = self._objCollection.getIcon(sqType)
+        try:
+            pixmap = QtGui.QPixmap(imgFile, "1")
+        except:
+            pixmap = QtGui.QPixmap(imgFile)
+        
+        transform = QtGui.QTransform().rotate(int(rotation))
+        rotatedPixmap = pixmap.transformed(transform, QtCore.Qt.SmoothTransformation)
+        
+        size = QSize(self._tilesize, self._tilesize)
+        scaledPixmap = rotatedPixmap.scaled(size, QtCore.Qt.KeepAspectRatio)
+        
+        label.setPixmap(scaledPixmap)
+
 #FIXME turn to widget. Possible memory leak
 class PropertiesItem():
-    def __init__(self, objModel, title, objCollection, mapModel, category):
+    def __init__(self, objModel, title, objCollection, mapModel, category, tilesize=64):
         self._model = objModel
         self._mapModel = mapModel
 
         self.widget = QGroupBox(title)
-        self.layout = QHBoxLayout()
+        self.layout = QVBoxLayout()
         self.widget.setLayout(self.layout)
+
+        # Add SimpleSquareItem before properties
+        self.simpleSquareItem = SimpleSquareItem(objModel, objCollection, tilesize)
+        self.layout.addWidget(self.simpleSquareItem)
 
         availableObjects = objCollection.getTypesInCategory(category)
 
@@ -106,4 +146,3 @@ class PropertiesPanel(QWidget):
     def removeObject(self, id):
         self.mapModel.deleteSquareById(id)
         self.showSquareProperties(self.x, self.y)
-
